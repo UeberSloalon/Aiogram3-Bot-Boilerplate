260f0a008d08a198adc91b6e976ba2ab545023df
UeberSloalon <saus4567@gmail.com>
2025-12-14 01:07:59 +0700

    feat: add config.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+from config import *
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-
 async def main():
     await dp.start_polling()
-
 
 if __name__ == '__main__':
     asyncio.run(main())
